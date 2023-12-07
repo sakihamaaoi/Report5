caaabdabe7cc16cfd011ac98ceb98a0615c5ebe1
Aoi Sakihama <e235723@ie.u-ryukyu.ac.jp>
2023-12-07 15:55:55 +0900

    1st commit

--- app/src/main/java/jp/ac/uryukyu/ie/e235723/Main.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e235723/Main.java
@@ -2,7 +2,12 @@ package jp.ac.uryukyu.ie.e235723;
 
 public class Main {
     public static void main(String[] args) {
-        String str = null;
-        System.out.println(str.length());    
+        try{    
+            String str = null;
+            System.out.println(str.length());
+        }catch(NullPointerException e){
+            System.out.println("NullPointerExceptionが発生しました");
+            System.out.println(e.getMessage());
+        }  
     }  
 }
